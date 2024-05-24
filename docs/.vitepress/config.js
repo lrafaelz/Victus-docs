af4534cbab8c8dd50983ede77b5df6bbe0ad9e02
Rafael Luz <rafaelmelo.aluno@unipampa.edu.br>
2024-05-24 07:54:21 -0300

    added idioms

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,12 +1,34 @@
-module.exports = {
-  title: 'Victus docs',
+import { defineConfig } from "vitepress"
+
+export default defineConfig({
+  lang: 'pt-BR',
+  lastUpdated: true,
+  locales: {
+    '/': {
+      lang: 'pt-BR',
+      title: 'Victus docs',
+      description: 'Documentação do projeto Victus Exergame'
+    },
+    '/en/': {
+      lang: 'en-US',
+      title: 'Victus docs',
+      description: 'Victus Exergame project documentation'
+    }
+  },
   themeConfig: {
+    localeLinks: {
+      text: '',
+      items: [
+        { text: 'Português', link: '/' },
+        { text: 'English', link: '/en/' }
+      ]
+    },
     nav: [
       { text: 'Sensoriamento', link: '/sensoriamento/' },
       { text: 'Milestones', link: '/milestones/' },
-      // { text: 'API', link: '/api/' },
-      // { text: 'Github', link: '
+      { text: 'Sobre', link: '/sobre/' },
+      { text: 'Github', link: '/github/' }
     ],
-    sidebar: []
   }
-}
+})
+  
